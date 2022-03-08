1304ef24430295b4a0ef82f07d64600e158e0b6a
PatelKavya21 <pkavya029@gmail.com>
2022-03-08 13:34:07 +0530

    submit

---
+++ Practical3/Student.cs
@@ -0,0 +1,20 @@
+﻿namespace Practical3
+{
+    internal class Student
+    {
+        internal void percentage()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void getdata()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void grade()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
